01ac02453713a0a8cc6c7a0ffd44ac8732542537
Lukas Theis <muwum@muwum-lexicons.de>
2014-01-27 19:32:31 +0100

    reset to data

--- scenarios/0_battleglest/player.lua
+++
@@ -1,4 +0,0 @@
-herosOfPlayersabc[1] = "arbol_alma"
-herosOfPlayersabc[2] = "golden_knight"
-herosOfPlayersabc[5] = "saurian"
-herosOfPlayersabc[6] = "bai_zaa"
--- scenarios/0_battleglest/unitAttacked.lua
+++
@@ -1,9 +0,0 @@
-if lastAttackedUnitName() == "tingorn" then
-	if units[lastAttackedUnit()] == nil then
-		print("Error: nil value");
-	else
-		if units[lastAttackedUnit()]:distance({ 180 , 330 }) > 7 then
-			givePositionCommand(units[lastAttackedUnit()].id, "move", { 180 , 330 })
-		end
-	end
-end
